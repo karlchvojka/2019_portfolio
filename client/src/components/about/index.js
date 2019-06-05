0938b679323a4a6d6bff46c0f5b553ee197245bd
Karl <karl.chvojka@gmail.com>
2019-06-05 08:28:17 -0400

    About section transferred to bootstrap

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import Head1 from '../../assets/head1.png';
 
 class About extends Component {
   render() {
     return (
-      <div id="about" className="aboutWrap">
-        <div>
-        <img className="aboutImg" alt="Karl Chvojka Headshot" src={Head1} />
-        <h2>About</h2>
-          <p className="firstP">I am a Full-stack developer with 10 years of industry experience building websites, advertising campaigns, interacting with traditional media, and working in teams of people from a variety of disciplines.</p>
-          <p> I have an unending curiosity for new ideas and processes that helps expand my work. the advancement of Technology Continues to bewilder and inspires me.</p>
-        </div>
+      <section id="about" className="aboutWrap">
+        <Container fluid="true">
+          <Row>
+            <Col xl={12} lg={12} md={12} sm={12} xs={12}>
+            <img className="aboutImg" alt="Karl Chvojka Headshot" src={Head1} />
+            <h2>About</h2>
+              <p className="firstP">I am a Full-stack developer with 10 years of industry experience building websites, advertising campaigns, interacting with traditional media, and working in teams of people from a variety of disciplines.</p>
+              <p> I have an unending curiosity for new ideas and processes that helps expand my work. the advancement of Technology Continues to bewilder and inspires me.</p>
+            </Col>
+          </Row>
+        </Container>
 
-      </div>
+      </section>
     );
   }
 }
