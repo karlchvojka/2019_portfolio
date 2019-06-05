996a7b6af1f5f726a569ba3c8668d3a8f5c14462
Karl <karl.chvojka@gmail.com>
2019-06-05 16:59:41 -0400

    Ran a build

---
+++ build/precache-manifest.fa4095ab98aff25f00c2b29d59e357b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70b52aa4cacfae11d5410e5681f7ee4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41dc5f9cfd1f6ed237da",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e9a65adb1d92fd8d70f5",
+    "url": "/static/css/main.a83c5077.chunk.css"
+  },
+  {
+    "revision": "41dc5f9cfd1f6ed237da",
+    "url": "/static/js/2.5895e511.chunk.js"
+  },
+  {
+    "revision": "e9a65adb1d92fd8d70f5",
+    "url": "/static/js/main.91a1d01d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ea3553d11ef055a9f60296facbf326c",
+    "url": "/static/media/chronicink-slider2.2ea3553d.jpg"
+  },
+  {
+    "revision": "0ff73a848379dd95fed1fa67c00dfa12",
+    "url": "/static/media/head1.0ff73a84.png"
+  },
+  {
+    "revision": "5a46b1d5dbba60f1af2d19477f5a0790",
+    "url": "/static/media/hexapixel_logo.5a46b1d5.jpg"
+  }
+]);
