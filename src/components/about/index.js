8288598881249f1d0aab4bc0b29baf73e968d4a3
Karl Chvojka <karl.chvojka@gmail.com>
2019-06-14 15:26:37 -0400

    fixed a grammar error

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -12,7 +12,7 @@ class About extends Component {
               <img className="aboutImg" alt="Karl Chvojka Headshot" src={Head1} />
               <h2>About</h2>
               <p className="firstP">I am a Full-stack developer with 10 years of industry experience building websites, advertising campaigns, interacting with traditional media, and working in teams of people from a variety of disciplines.</p>
-              <p> I have an unending curiosity for new ideas and processes that helps expand my work. the advancement of Technology Continues to bewilder and inspires me.</p>
+              <p> I have an unending curiosity for new ideas and processes that helps expand my work. The advancement of Technology Continues to bewilder and inspires me.</p>
             </Col>
           </Row>
         </Container>
