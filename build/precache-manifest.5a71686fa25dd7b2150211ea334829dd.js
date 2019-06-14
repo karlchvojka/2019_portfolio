403f86d0c1222dc769dacd35c235f3dff8d30387
Karl Chvojka <karl.chvojka@gmail.com>
2019-06-14 10:22:54 -0400

    Fixing build errors

---
+++ build/precache-manifest.5a71686fa25dd7b2150211ea334829dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57535cd4ff85b85c2ec8e71a56d7bab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0809bbb012ae80acd0f1",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e6576ae9f831122acce8",
+    "url": "/static/css/main.1c8acd4c.chunk.css"
+  },
+  {
+    "revision": "0809bbb012ae80acd0f1",
+    "url": "/static/js/2.09586569.chunk.js"
+  },
+  {
+    "revision": "e6576ae9f831122acce8",
+    "url": "/static/js/main.90e47588.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2ea3553d11ef055a9f60296facbf326c",
+    "url": "/static/media/chronicink-slider2.2ea3553d.jpg"
+  },
+  {
+    "revision": "0ff73a848379dd95fed1fa67c00dfa12",
+    "url": "/static/media/head1.0ff73a84.png"
+  },
+  {
+    "revision": "5a46b1d5dbba60f1af2d19477f5a0790",
+    "url": "/static/media/hexapixel_logo.5a46b1d5.jpg"
+  }
+]);
