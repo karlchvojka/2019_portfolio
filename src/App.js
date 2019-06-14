2f2953c8a079625293bc0a0539f8d5d476e8b98f
Karl Chvojka <karl.chvojka@gmail.com>
2019-06-14 09:42:02 -0400

    Fixing some build errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import ReactGA from 'react-ga';
-import createHistory from 'history/createBrowserHistory'
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/Home/';
 import Resume from './components/Resume/';
@@ -9,18 +8,21 @@ import About from './components/about/';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.scss'
 
-const history = createHistory()
-ReactGA.initialize('UA-18900659-1');
 history.listen((location, action) => {
     ReactGA.pageview(location.pathname + location.search);
     console.log(location.pathname)
 });
 
+function initializeReactGA() {
+    ReactGA.initialize('UA-18900659-1');
+    ReactGA.pageview('/');
+}
+
 
 class App extends Component {
   render() {
     return (
-      <Router history={history}>
+      <Router>
         <Route path="/" exact component={ Home } />
         <Route path="/about" exact component={ About } />
         <Route path="/resume" exact component={ Resume } />
